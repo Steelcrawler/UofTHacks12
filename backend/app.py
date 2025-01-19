d6d526d26f201df5ef53620c9808fc3b638ff3bd
avantitandon <157421594+avantitandon@users.noreply.github.com>
2025-01-18 19:21:47 -0500

    added chat bot integration

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from mongodb_interface import MongoDBInterface
+from gcp.gcpchatbotintegrated import ChatSessionManager
 
 app = Flask(__name__)
 CORS(app)
-
-
-# Initialize MongoDBInterface
 mongo_interface = MongoDBInterface()
+session_manager = ChatSessionManager()
 
 @app.route('/api/register', methods=['POST'])
 def register_user():
@@ -18,7 +17,7 @@ def register_user():
     # Call create_user method to register the user
     if not email or not password:
         return jsonify({"message": "Email and password are required"}), 400
-    
+
     if mongo_interface.create_user(email, password):
         return jsonify({"message": "User created successfully"}), 200
     else:
@@ -49,12 +48,16 @@ def handle_submission():
 
     if not data or 'submittedText' not in data:
         return jsonify({"error": "Invalid data"}), 400
-    
+
     submitted_text = data['submittedText']
-    
     print(f"Received text: {submitted_text}")
-    
-    return jsonify({"message": "Submission successful", "receivedText": submitted_text}), 200
+
+    try:
+        # Pass the input to the chatbot and get the response
+        bot_response = session_manager.send_message(submitted_text)
+        return jsonify({"message": "Submission successful", "botResponse": bot_response}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 50
 
 if __name__ == '__main__':
     app.run(debug=True)
