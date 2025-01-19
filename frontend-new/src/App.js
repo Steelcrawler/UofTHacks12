1e97171b281937ac689bfb0c64b99d94b4aa95cb
Adventurer-E <erickudou@gmail.com>
2025-01-18 20:46:18 -0500

    About

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -3,10 +3,12 @@ import ScrollTriggered from './ScrollTriggered'
 import NavBar from './NavBar';
 import './App.css';
 import LoginRegisterWindow from './LoginRegisterWindow';
+import About from './About';
 
 
 function App() {
   const [showLoginWindow, setShowLoginWindow] = useState(false);
+  const [showAboutWindow, setShowAboutWindow] = useState(true);
   const toggleLoginWindow = (e) => {
     e.preventDefault(); // Prevent the default anchor link behavior
     setShowLoginWindow(!showLoginWindow);
@@ -15,12 +17,22 @@ function App() {
   const closeLoginWindow = () => {
     setShowLoginWindow(false);
   };
+
+  const toggleAboutWindow = (e) => {
+    e.preventDefault(); // Prevent the default anchor link behavior
+    setShowAboutWindow(!showAboutWindow);
+  };
+
+  const closeAboutWindow = () => {
+    setShowAboutWindow(false);
+  };
   
   return (
     <div className="App">
-      <NavBar onLoginClick={toggleLoginWindow} />
+      <NavBar onLoginClick={toggleLoginWindow} onAboutClick={toggleAboutWindow} />
       <ScrollTriggered />
       {showLoginWindow && <LoginRegisterWindow onClose={closeLoginWindow} />}
+      {showAboutWindow && <About onClose={closeAboutWindow} />}
     </div>
   )
 }
--- frontend-new/src/NavBar.js
+++ frontend-new/src/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './NavBar.css';
 import logo from './logo.png'; // necessary?
 
-const NavBar = ({ onLoginClick }) => {
+const NavBar = ({ onLoginClick, onAboutClick }) => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={logo} className="navbar-logo-img" alt="logo" />
       </div>
       <ul className="navbar-links">
-        <li><a href="#about">About</a></li>
+        <li><a onClick={onAboutClick}>About</a></li>
         <li><a href="#chat">Chat</a></li>
         <li><a onClick={onLoginClick}>Login/Register</a></li>
       </ul>
