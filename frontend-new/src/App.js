33e183c957e23acc01ec6d138cf295d4cfcc46ec
codymavok <85233381+codymavok@users.noreply.github.com>
2025-01-18 22:27:18 -0500

    Chrome extension added

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import ScrollTriggered from './ScrollTriggered'
+import ExtensionWrapper from './ExtensionWrapper';
 import NavBar from './NavBar';
 import './App.css';
 import LoginRegisterWindow from './LoginRegisterWindow';
@@ -7,22 +8,35 @@ import LoginRegisterWindow from './LoginRegisterWindow';
 
 function App() {
   const [showLoginWindow, setShowLoginWindow] = useState(false);
+  
+  const isExtension = window.chrome && window.chrome.runtime && window.chrome.runtime.id;
+
   const toggleLoginWindow = (e) => {
-    e.preventDefault(); // Prevent the default anchor link behavior
+    e.preventDefault();
     setShowLoginWindow(!showLoginWindow);
   };
 
   const closeLoginWindow = () => {
     setShowLoginWindow(false);
   };
-  
+
+  if (isExtension) {
+    return (
+      <div className="App">
+        <ExtensionWrapper>
+          <ScrollTriggered />
+        </ExtensionWrapper>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <NavBar onLoginClick={toggleLoginWindow} />
       <ScrollTriggered />
       {showLoginWindow && <LoginRegisterWindow onClose={closeLoginWindow} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
